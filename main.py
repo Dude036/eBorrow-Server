def7e5be7063ba03cfb0f96713a4f95bea21e31e
Josh Higham <josh@higham.com>
2019-01-25 15:59:05 -0700

    Adding prereq's. not quite done testing

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def main():
 	global decode_buffer
 	HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 	PORT = 41111        # Port to listen on (non-privileged ports are > 1023)
+	data=''
 
 	while True:
 		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
