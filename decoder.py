04eae809024cc6690b86bcbd1fc890d155b95830
Tim Weber <tim.weber@aggiemail.usu.edu>
2019-04-16 17:48:12 -0600

    decoder changes

--- decoder.py
+++ decoder.py
@@ -413,6 +413,9 @@ def piped(username, packet_id, packet, addr, transmit_buffer):
                     transmit_buffer.put([error_handler(7), addr])
                     return
                 user.add_pending_exchange(key, packet)
+                lender = retrieve_user(lender_name)
+                lender.add_pending_exchange(key, packet)
+                transmit_buffer.put([error_handler(0), addr])
             else:
                 logging.error("DECODER :: Username: '" + borrow_name + "' has incorrect public Key")
                 transmit_buffer.put([error_handler(4), addr])
