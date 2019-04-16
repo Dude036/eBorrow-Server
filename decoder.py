6d01e9b585c8165ef539968267c764b2977ef2ba
Tim Weber <tim.weber@aggiemail.usu.edu>
2019-04-16 16:55:17 -0600

    Forgot a colon

--- decoder.py
+++ decoder.py
@@ -448,7 +448,7 @@ def piped(username, packet_id, packet, addr, transmit_buffer):
             transmit_buffer.put([error_handler(17), addr])
             return
         sender_key = packet["Key"]
-        if verify_key(sender_name, sender_key, public=True)
+        if verify_key(sender_name, sender_key, public=True):
             transmit_buffer.put(packet)
         else:
             logging.error("DECODER :: Username: '" + sender_name + "' has incorrect public Key")
